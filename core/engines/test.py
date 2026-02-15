b1b290090e9bd395dc6a8ee7419ebdcf41111678
wiesiekbiznesowe-gif <wiesiekbiznesowe@gmail.com>
2026-02-16 00:10:20 +0100

    Update test.py

--- core/engines/test.py
+++ core/engines/test.py
@@ -1,5 +1,9 @@
-class Engine:
-    def run(self, payload):
+from core.base_engine import BaseEngine
+
+
+class Engine(BaseEngine):
+
+    def run(self, payload: dict) -> dict:
         return {
             "status": "success",
             "received": payload
