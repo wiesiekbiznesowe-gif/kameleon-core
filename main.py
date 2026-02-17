44d84099dea13baffdb06e55fbe6dfc014f2fdfe
wiesiekbiznesowe-gif <wiesiekbiznesowe@gmail.com>
2026-02-17 22:15:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from core.orchestrator import Orchestrator
+from core.engines.test import Engine
+
 
 def main():
     orchestrator = Orchestrator()
+
+    orchestrator.register_module("test", Engine())
+
     result = orchestrator.execute("test", {"message": "Start Kameleon"})
     print(result)
+
 
 if __name__ == "__main__":
     main()
