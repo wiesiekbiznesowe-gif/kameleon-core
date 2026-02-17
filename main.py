3caa24b1e6c6ba0430cd876baf42cfcf04800d0d
wiesiekbiznesowe-gif <wiesiekbiznesowe@gmail.com>
2026-02-17 22:57:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from core.orchestrator import Orchestrator
 from core.engines.test import Engine
+from core.engines.echo_engine import EchoEngine
 
 
 def main():
     orchestrator = Orchestrator()
 
+    # Rejestrujemy silniki
     orchestrator.register_module("test", Engine())
+    orchestrator.register_module("echo", EchoEngine())
 
-    result = orchestrator.execute("test", {"message": "Start Kameleon"})
-    print(result)
+    # Test 1
+    result1 = orchestrator.execute("test", {"message": "Start Kameleon"})
+    print(result1)
+
+    # Test 2
+    result2 = orchestrator.execute("echo", {"message": "Kameleon rośnie"})
+    print(result2)
 
 
 if __name__ == "__main__":
