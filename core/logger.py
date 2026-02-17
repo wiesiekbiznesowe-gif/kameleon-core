d9f795a968240d1371c802c8f7e672dd4d097d53
wiesiekbiznesowe-gif <wiesiekbiznesowe@gmail.com>
2026-02-17 23:17:25 +0100

    Update logger.py

--- core/logger.py
+++ core/logger.py
@@ -1,1 +1,9 @@
+from datetime import datetime
 
+
+class CoreLogger:
+
+    @staticmethod
+    def log(message: str) -> None:
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        print(f"[{timestamp}] {message}")
