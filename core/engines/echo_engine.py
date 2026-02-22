6eb586f0126dbc7936a675aae1ffb0fde6996764
wiesiekbiznesowe-gif <wiesiekbiznesowe@gmail.com>
2026-02-22 12:57:33 +0100

    Update echo_engine.py

--- core/engines/echo_engine.py
+++ core/engines/echo_engine.py
@@ -1,24 +1,10 @@
 from core.base_engine import BaseEngine
 
+
 class EchoEngine(BaseEngine):
 
-    def run(self, payload: dict) -> dict:
-        try:
-            text = payload.get("message", "")
-
-            return {
-                "status": "success",
-                "engine": "echo",
-                "data": {
-                    "echo": text.upper()
-                },
-                "error": None
-            }
-
-        except Exception as e:
-            return {
-                "status": "error",
-                "engine": "echo",
-                "data": None,
-                "error": str(e)
-            }
+    def _execute(self, payload: dict):
+        text = payload.get("message", "")
+        return {
+            "echo": text.upper()
+        }
