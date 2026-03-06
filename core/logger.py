0fd7bdf6532441f1c0d89e15140ab522c3fcbe44
wiesiekbiznesowe-gif <wiesiekbiznesowe@gmail.com>
2026-03-07 00:28:42 +0100

    Update logger.py

--- core/logger.py
+++ core/logger.py
@@ -1,32 +1,10 @@
-import logging
-import os
+from datetime import datetime
 
 
-class CoreLogger:
+class Logger:
 
-    LOG_DIR = "logs"
-    LOG_FILE = "logs/system.log"
+    def log(self, engine, payload):
 
-    @staticmethod
-    def _ensure_log_dir():
-        if not os.path.exists(CoreLogger.LOG_DIR):
-            os.makedirs(CoreLogger.LOG_DIR)
-
-    @staticmethod
-    def get_logger():
-        CoreLogger._ensure_log_dir()
-
-        logger = logging.getLogger("KAMELEON_CORE")
-
-        if not logger.handlers:
-            logger.setLevel(logging.INFO)
-
-            file_handler = logging.FileHandler(CoreLogger.LOG_FILE)
-            formatter = logging.Formatter(
-                "%(asctime)s | %(levelname)s | %(message)s"
-            )
-            file_handler.setFormatter(formatter)
-
-            logger.addHandler(file_handler)
-
-        return logger
+        print(
+            f"[LOG {datetime.utcnow()}] engine={engine} payload={payload}"
+        )
